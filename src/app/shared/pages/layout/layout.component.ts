0015de6c35a115be542f661a5e2cf0f05213bec6
Klis18 <klisnp18@gmail.com>
2025-05-29 15:47:02 -0500

    primeras configuraciones

---
+++ src/app/shared/pages/layout/layout.component.ts
@@ -0,0 +1,74 @@
+import {MediaMatcher} from '@angular/cdk/layout';
+
+import { Component, inject, OnDestroy, signal } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  templateUrl: './layout.component.html',
+  styles: `
+    .background-dark{
+      background-color: black !important;
+      border-radius: 0 !important;
+      width: 20%;
+    }
+
+    .example-container {
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+
+    .example-is-mobile .example-toolbar {
+      position: fixed;
+      z-index: 2;
+    }
+
+    .example-sidenav-container {
+      flex: 1;
+    }
+
+    .example-is-mobile .example-sidenav-container {
+      flex: 1 0 auto;
+    }
+  `
+})
+export class LayoutComponent implements OnDestroy{
+  protected readonly fillerNav = ['Panel de Control','Tareas', 'Eventos'];
+
+  protected readonly fillerContent = 'Hola, Bienvenido !!! XD';
+
+  protected readonly isMobile = signal(true);
+
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
+
+  constructor() {
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
+
+}
